8db0af4e594bd494d66ed645bc242e1bb7708547
Pradeep <pradhi.sjv@gmail.com>
2018-06-27 11:04:43 +0530

    qwinixSlingGinInitilizecopy

---
+++ src/main/java/com/qwinix/QwinixSling/QwinixSlingApplication.java
@@ -0,0 +1,12 @@
+package com.qwinix.QwinixSling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwinixSlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwinixSlingApplication.class, args);
+	}
+}
